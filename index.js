edc7383fdc8d5e809eb2a5b72c5b88202375faf4
MHusban <qasiraslam64@gmail.com>
2025-12-07 11:29:25 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,76 @@
+// --1-ALERTS
+// PART 1
+// alert("Hello,World")
+
+// PART 2
+// alert("Husban Aslam")
+
+// PART 3
+// alert(1234)
+
+// PART 4 
+// const message = "HelloHowAreYOU"
+// alert(message)
+
+// PART 5
+// var Name = " Husban"
+// var greetings = " As-Salamu Alaykum" + Name
+// alert(greetings)
+
+
+
+// --2-VARIABLES FOR STRINGS
+
+// PART 1
+// const userName= "M.Husban"
+// console.log(userName)
+
+// PART 2
+// const welcomeMessage = "Hello,Sir How are you"
+// alert(welcomeMessage)
+
+// PART 3
+// var firstName = "Husban"
+// var lastName = " Aslam"
+// var fullName = firstName + lastName
+// console.log(fullName)
+
+// PART 4
+// var favouriteQuote = "If you belive in YOURSELF Anything is POSSIBLE"
+// console.log(favouriteQuote)
+
+// PART 5
+// const city = "Karachi"
+// alert("Welcome to " + city +"!")
+
+
+
+// --3-VARIABLES FOR NUMBER
+
+// PART 1
+// const age = 16
+// console.log(age)
+
+// PART 2
+// let x = 6
+// let y = 4
+// console.log(x + y)
+
+// PART 3
+// let x = 6
+// let y = 4
+// console.log(x - y)
+
+// PART 4
+// let x = 6
+// let y = 4
+// console.log(x * y)
+
+// PART 5
+// const messagePrePhase = "You are "
+// const age = "16 "
+// const messageLastPhase = "years old !"
+// console.log(messagePrePhase + age + messageLastPhase)
+
+
+
