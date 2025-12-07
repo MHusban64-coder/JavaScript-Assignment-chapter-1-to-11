8f195aad09dd8118f02c308c5eec54b00689666d
MHusban <qasiraslam64@gmail.com>
2025-12-07 13:56:21 +0500

    second and last commit

--- index.js
+++ index.js
@@ -74,3 +74,124 @@
 
 
 
+// --4-MATH EXPRESSIONS
+
+// PART 1
+// let x = 10
+// let y = 5
+// console.log(x + y)
+
+// PART 2
+// let x = 10
+// let y = 5
+// alert(x - y)
+
+// PART 3 
+// let x = 10
+// let y = 5
+// console.log(x * y)
+
+// PART 4
+// let x = 10
+// let y = 5
+// console.log(x / y)
+
+// PART 5
+// let x = 10
+// let y = 5
+// console.log(x % y)
+
+
+
+// --5-PROMPT:GETTING INPUT FROM USER
+
+// PART 1
+// var userName = prompt("What is your Name")
+// var greetings = "Welcome ,Sir "
+// var end = " !"
+// alert(greetings + userName + end)
+
+// PART 2
+// var userAge = prompt("What is your Age")
+// var statement = "Your Age is "
+// var years = " years !"
+// alert(statement + userAge + years)
+
+// PART 3
+// var number = Number(prompt("give us a Number , we double it"))
+// var answerStatement = "The answer is "
+// alert(number + number)
+
+// PART 4
+// var beforeStatement = "Wow! I like "
+// var userColor = prompt("Enter your color")
+// var afterstatement = " too!"
+// alert(beforeStatement + userColor + afterstatement)
+
+// PART 5
+// var beforeStatement = "You are from "
+// var city = prompt("Enter your City")
+// var afterstatement = " !"
+// alert(beforeStatement + city + afterstatement)
+
+
+
+// --6-IF STATEMETEMENT AND COMPARISION OPERATOR
+
+// PART 1
+// var userAge = Number(prompt("Enter your Age"))
+// var answerLess30 = "You are still young"
+// var answerAbove30 = "You are ended"
+// if (userAge <= 30) {
+//     alert(answerless30)
+// } 
+// else(alert(answerabove30))
+
+// PART 2
+// var number = prompt("Enter a number")
+// var result1 = "Even number"
+// var result2 ="odd number"
+// if (number % 2==0) {
+//     alert(result1)
+// } else {
+//     alert(result2)
+// }
+
+// PART 3
+// var firstNumber = prompt("Enter your First Number")
+// var secondNumber = prompt("Enter your Second Number")
+// var statement = "The greater Number is "
+// if (firstNumber > secondNumber) {
+//     alert(statement + firstNumber)
+// } else {
+//     alert(statement + secondNumber)
+// }
+
+// PART 4
+// var password = 12345678
+// var enteredPassword = prompt("Enter your password")
+// if (enteredPassword==password) {
+//     alert("Password Correct")
+// } else {
+//      alert("Password Incorrect")
+// }
+
+// PART 5
+
+// let x = 10;
+// let  = "10";
+
+// // loose equality (true)
+// console.log(x == y)
+
+// // loose equality (true)
+// console.log(x === y)
+
+// // loose inequality (false)
+// console.log(x != y)
+
+// // strict inequality (true)
+// console.log(x !== y);  
+
+
+// <<<<=============!!completed!!=============>>>>
